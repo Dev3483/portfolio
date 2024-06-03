d55f02ac897c8dfdc962609c92680fadd885f451
Dev3483 <168566273+Dev3483@users.noreply.github.com>
2024-06-03 17:37:37 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/rapeseeds-7165185_1280.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "girl ride cycle in flower garden";
 
 /**
  * Project list
