89f0477256b77ae106d12b9ff11d9177db5be687
Dev3483 <168566273+Dev3483@users.noreply.github.com>
2024-06-03 17:23:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Dev Mali",
+  title: "Web Developer",
+  email: "devmali.552005@gmail.com",
+  gitHub: "Dev3483",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "Dev Mali",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
